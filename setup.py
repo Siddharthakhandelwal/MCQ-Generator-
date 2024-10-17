11b237518cc6c8de5a7b8227fbcbbcf2c9a97aaf
Siddharthakhandelwal <siddharthakhandelwal9@gmail.com>
2024-10-18 01:09:52 +0530

    final MCQ generator

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='1.0.5',
-    author='Keshav Agrawal',
-    author_email='tekionix.askkeshav@gmail.com',
+    author='Siddhartha",
+    author_email='siddharthakhandelwal9@gmail.com',
     install_requires=["google-generativeai","langchain","streamlit","python-dotenv","PyPDF2","langchain-google-genai"],
     packages=find_packages()
 )
